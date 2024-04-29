8ab27cd317706102393a32c8eff6739f5932a5c3
Christian Lozano <christian.lozano@neoris.com>
2024-04-29 11:38:57 +0200

    [SOLVE] Solucion CHR - Solucion final

--- src/org/formacion/builder/Persona.java
+++ src/org/formacion/builder/Persona.java
@@ -23,23 +23,61 @@ public class Persona {
 			persona.municipio = municipio;
 			return this;
 		}
-		
-		public Builder setMayor(int edad, String lugarTrabajo) {
+
+		public BuilderMayor setMayor(int edad) {
 			if (edad < 18) throw new IllegalArgumentException("es menor de edad " + edad);
 			persona.edad = edad;
-			persona.lugarTrabajo = lugarTrabajo;
 			persona.colegio = null;
-			return this;
+			return new BuilderMayor(persona);
 		}
-		
-		public Builder setMenor(int edad, String colegio) {
+
+		public BuilderMenor setMenor(int edad) {
 			if (edad >= 18) throw new IllegalArgumentException("es mayor de edad " + edad);
 			persona.edad = edad;
-			persona.colegio = colegio;
-			persona.lugarTrabajo = null;
+			return new BuilderMenor(persona);
+		}
+
+		public Builder setLugarTrabajo (String lugarTrabajo) {
+			persona.lugarTrabajo = lugarTrabajo;
 			return this;
 		}
-	
+
+		public Persona build() {
+			return persona;
+		}
+
+	}
+
+	public static class BuilderMenor {
+		private Persona persona;
+
+		public BuilderMenor(Persona persona) {
+			this.persona = persona;
+		}
+
+		public BuilderMenor setColegio(String colegio) {
+			persona.colegio = colegio;
+			return this;
+		}
+
+		public Persona build() {
+			return persona;
+		}
+
+	}
+
+	public static class BuilderMayor {
+		private Persona persona;
+
+		public BuilderMayor(Persona persona) {
+			this.persona = persona;
+		}
+
+		public BuilderMayor setLugarTrabajo(String lugarTrabajo) {
+			persona.lugarTrabajo = lugarTrabajo;
+			return this;
+		}
+
 		public Persona build() {
 			return persona;
 		}
